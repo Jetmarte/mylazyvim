78a6cf41ea2dce5d59ca070ee5eea7ae9b667519
Jetmarte <obcollazos@gmail.com>
2025-02-06 19:38:33 -0400

    agregar colores a los separadores

--- init.lua
+++ init.lua
@@ -34,20 +34,20 @@ vim.opt.guifont = { "FiraCode Nerd Font:h12" }
 vim.keymap.set("n", "q", "<Nop>", { noremap = true, silent = true })
 
 -----cambiar barra numerica segun el modo--------
--- vim.cmd("highlight LineNr guifg=#86c7f0") -- Color para el modo Normal
--- vim.api.nvim_create_autocmd("ModeChanged", {
---   pattern = "*",
---   callback = function()
---     local mode = vim.fn.mode()
---     if mode == "n" then
---       -- vim.cmd("hi Normal guibg=#06171f") -- Fondo para modo Normal
---       vim.cmd("highlight LineNr guifg=#86c7f0") -- Color para el modo Normal
---     elseif mode == "i" then
---       -- vim.cmd("hi Normal guibg=#061f0d") -- Fondo para modo Insert
---       vim.cmd("highlight LineNr guifg=#dfe3af") -- Color para el modo Normal
---     elseif mode == "v" or mode == "V" or mode == "" then
---       -- vim.cmd("hi Normal guibg=#2e1509") -- Fondo para modo Visual
---       vim.cmd("highlight LineNr guifg=#e6aeae") -- Color para el modo Normal
---     end
---   end,
--- })
+vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#0f3b8c", bg = "None" }) -- Rojo
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "*",
+  callback = function()
+    local mode = vim.fn.mode()
+    if mode == "n" then
+      vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#165d94", bg = "None" })
+    elseif mode == "i" then
+      vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#219416", bg = "None" })
+    elseif mode == "v" or mode == "V" or mode == "" then
+      vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { fg = "#ff0000", bg = "None" })
+    end
+  end,
+})
+
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#e6aeae", bg = "None" }) -- Rojo
