0a54041bf1aa3db696f4ceaf17e5f4af1b28647f
Jetmarte <obcollazos@gmail.com>
2026-02-01 13:53:19 -0400

    dap nueva config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,60 +1,130 @@
--- debug code like vscode
+-- extencion para debugear
+--  : MasonInstall js-debug-adapter
 return {
   "mfussenegger/nvim-dap",
-  recommended = true,
-  desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
-
   dependencies = {
     "rcarriga/nvim-dap-ui",
-    -- virtual text for the debugger
+    "theHamsta/nvim-dap-virtual-text",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+
+  keys = {
     {
-      "theHamsta/nvim-dap-virtual-text",
-      opts = {},
+      "<leader>db",
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      desc = "Toggle Breakpoint",
+    },
+    {
+      "<leader>dB",
+      function()
+        require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end,
+      desc = "Conditional Breakpoint",
+    },
+
+    {
+      "<leader>dc",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Continue",
+    },
+    {
+      "<leader>di",
+      function()
+        require("dap").step_into()
+      end,
+      desc = "Step Into",
+    },
+    {
+      "<leader>dO",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Step Over",
+    },
+    {
+      "<leader>do",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Step Out",
+    },
+
+    {
+      "<leader>dr",
+      function()
+        require("dap").repl.toggle()
+      end,
+      desc = "Toggle REPL",
+    },
+    {
+      "<leader>dt",
+      function()
+        require("dap").terminate()
+      end,
+      desc = "Terminate",
+    },
+    {
+      "<leader>du",
+      function()
+        require("dapui").toggle()
+      end,
+      desc = "Toggle UI",
     },
   },
 
-  -- stylua: ignore
-  keys = {
-    { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
-    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
-    { "<leader>dc", function() require("dap").continue() end, desc = "Run/Continue" },
-    { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
-    { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
-    { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
-    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
-    { "<leader>dj", function() require("dap").down() end, desc = "Down" },
-    { "<leader>dk", function() require("dap").up() end, desc = "Up" },
-    { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
-    { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
-    { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
-    { "<leader>dP", function() require("dap").pause() end, desc = "Pause" },
-    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
-    { "<leader>ds", function() require("dap").session() end, desc = "Session" },
-    { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
-    { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
-  },
-
   config = function()
-    -- load mason-nvim-dap here, after all adapters have been setup
-    if LazyVim.has("mason-nvim-dap.nvim") then
-      require("mason-nvim-dap").setup(LazyVim.opts("mason-nvim-dap.nvim"))
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    dapui.setup()
+    require("nvim-dap-virtual-text").setup()
+
+    dap.adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        args = {
+          vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+          "${port}",
+        },
+      },
+    }
+
+    dap.configurations.javascript = {
+      {
+        name = "Debug Next.js (Server)",
+        type = "pwa-node",
+        request = "launch",
+        program = "${workspaceFolder}/node_modules/.bin/next",
+        args = { "dev" },
+        cwd = "${workspaceFolder}",
+        sourceMaps = true,
+        protocol = "inspector",
+        console = "integratedTerminal",
+      },
+    }
+
+    -- Auto UI open/close
+    dap.listeners.after.event_initialized["dapui"] = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated["dapui"] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui"] = function()
+      dapui.close()
     end
 
-    vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-
-    for name, sign in pairs(LazyVim.config.icons.dap) do
-      sign = type(sign) == "table" and sign or { sign }
-      vim.fn.sign_define(
-        "Dap" .. name,
-        { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
-      )
-    end
-
-    -- setup dap config by VsCode launch.json file
+    -- VSCode launch.json support
     local vscode = require("dap.ext.vscode")
-    local json = require("plenary.json")
     vscode.json_decode = function(str)
-      return vim.json.decode(json.json_strip_comments(str))
+      return vim.json.decode(require("plenary.json").json_strip_comments(str))
     end
   end,
 }
