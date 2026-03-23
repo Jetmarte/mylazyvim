77069093bee6c7ffca39f05d18674d35697bd2fc
Jetmarte <obcollazos@gmail.com>
2026-03-23 00:59:26 -0400

    config neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,16 +9,125 @@ return {
   lazy = false,
 
   opts = {
+    close_if_last_window = true,
+
+    default_component_configs = {
+      indent = {
+        indent_size = 2,
+        padding = 1,
+        with_markers = true,
+        indent_marker = "│",
+        last_indent_marker = "└",
+        with_expanders = true,
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+      icon = {
+        folder_closed = "",
+        folder_open = "",
+        folder_empty = "󰜌",
+        default = "󰈙",
+      },
+      modified = {
+        symbol = "●",
+      },
+      name = {
+        trailing_slash = false,
+        highlight_opened_files = true,
+      },
+      git_status = {
+        symbols = {
+          added = "✚",
+          modified = "",
+          deleted = "✖",
+          renamed = "󰁕",
+          untracked = "",
+          ignored = "",
+          unstaged = "󰄱",
+          staged = "",
+          conflict = "",
+        },
+      },
+    },
+
     window = {
+      position = "left",
+      width = 36,
       mappings = {
         ["Y"] = function(state)
           local node = state.tree:get_node()
           local path = node:get_id()
-          local cwd = vim.loop.cwd()
           local rel = vim.fn.fnamemodify(path, ":.")
-
           vim.fn.setreg("+", rel)
           vim.notify("Ruta copiada: " .. rel)
+        end,
+        ["gY"] = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          vim.fn.setreg("+", path)
+          vim.notify("Ruta absoluta copiada: " .. path)
+        end,
+        ["O"] = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+        end,
+        ["h"] = "close_node",
+        ["l"] = "open",
+        ["s"] = "open_split",
+        ["v"] = "open_vsplit",
+        ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+        ["E"] = "expand_all_nodes",
+        ["W"] = "close_all_nodes",
+      },
+    },
+
+    filesystem = {
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          "node_modules",
+          ".git",
+          ".DS_Store",
+          "thumbs.db",
+        },
+        never_show = {
+          ".DS_Store",
+          "thumbs.db",
+        },
+      },
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = true,
+      },
+      use_libuv_file_watcher = true,
+      group_empty_dirs = true,
+    },
+
+    buffers = {
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = true,
+      },
+      group_empty_dirs = true,
+      show_unloaded = true,
+    },
+
+    git_status = {
+      window = {
+        position = "float",
+      },
+    },
+
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.opt_local.number = false
+          vim.opt_local.relativenumber = false
         end,
       },
     },
