2e8d06092722706079726e7670fe184364fbe67a
Jetmarte <obcollazos@gmail.com>
2026-07-27 15:26:52 -0400

    configurado marckdonw previw

--- lua/mycode/myconfig/keymaps/markdown.lua
+++ lua/mycode/myconfig/keymaps/markdown.lua
@@ -2,4 +2,8 @@ function Markdown()
   --markdown--
   vim.api.nvim_set_keymap("n", "<leader>mr", ":RenderMarkdown<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("n", "<leader>mf", ":RenderMarkdownRefresh<CR>", { noremap = true, silent = true })
+
+  -- MarkdownPreview
+  vim.api.nvim_set_keymap("n", "<leader>mp", ":MarkdownPreview<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", "<leader>ms", ":MarkdownPreviewStop<CR>", { noremap = true, silent = true })
 end
